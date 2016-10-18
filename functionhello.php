865562c35f81cd2b59b97b70c04c938f16eac937
shitsukane <julianashitsukane@gmail.com>
2016-10-18 16:46:37 +0300

    Add files via upload

---
+++ functionhello.php
@@ -0,0 +1,16 @@
+<html>
+<body >
+<style>
+body {
+    background-color: lightblue;
+}
+</style>
+<?php
+function writeMsg() {
+    echo "Hello world!";
+}
+
+writeMsg(); // call the function
+?>
+</body>
+</html>
