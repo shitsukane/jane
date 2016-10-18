e191fc93d4618a57493c4d44329095a21941547d
shitsukane <julianashitsukane@gmail.com>
2016-10-18 16:47:22 +0300

    Add files via upload

---
+++ printperson.php
@@ -0,0 +1,25 @@
+<html>
+<body >
+<style>
+body {
+    background-color: lightblue;
+}
+</style>
+<?php
+function printPerson($name,$dob){
+
+echo $name ." ".$dob;
+
+
+}
+
+printPerson("Jane","1994</br>");
+echo "</br>";
+
+printPerson("Job","1995");
+echo "</br>";
+
+printPerson("Felix","1994");
+?>
+</body>
+</html>
